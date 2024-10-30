a742ae796298d482ff43637feb64b3151aab24a9
vidulunia <luniavidushi@gmail.com>
2024-10-30 12:08:41 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 const express = require ('express');
-const app = express();
-app.use((req,res) => {
-    res.send("Hello");
-})
 
+const app = express();
+
+const {adminAuth , userAuth} = require ("./middleware/auth");
+
+app.use("/admin" , adminAuth );
+
+app.post("user/login" , (req,res) => {
+    res.send("User logged in successfully");
+});
+
+app.get("/user" , userAuth, (req,res) =>{
+    res.send("User data sent");
+});
+
+app.get("/admin/getAlldata" , (req,res) => {
+    res.send("All data sent");   
+});
+
+app.get("/admin/deleteuser" , (req,res) => {
+    res.send("Deleted user");
+});
 app.listen(7777, () => {
     console.log("Server is successfully running on port 7777...");
 });
